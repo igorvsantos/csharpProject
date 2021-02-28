00efe2b6768760f2d289a3472f6a17354b434bdb
igorvsantos <71786817+igorvsantos@users.noreply.github.com>
2021-02-28 22:25:17 +0000

    Add files via upload

---
+++ Projeto/Movie4AllCsharp/Movie4AllCsharp/Show/Episodio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Movie4AllCsharp
+{
+    public class Episodio
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+        public DateTime Data { get; set; }
+
+        public Episodio()
+        {
+
+        }
+
+        public Episodio(int id, string nome, int numero, DateTime data)
+        {
+            Id = id;
+            Nome = nome;
+            Numero = numero;
+            Data = data;
+        }
+    }
+}
---
+++ Projeto/Movie4AllCsharp/Movie4AllCsharp/Show/Temporada.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Movie4AllCsharp
+{
+        public class Temporada
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Numero { get; set; }
+        public List<Episodio> Episodios { get; set; }
+
+        public Temporada()
+        {
+
+        }
+
+        public Temporada(int id, string nome, int numero)
+        {
+            Id = id;
+            Nome = nome;
+            Numero = numero;
+            Episodios = new List<Episodio>();
+        }
+    }
+}
